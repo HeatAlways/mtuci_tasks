e48f3710ecf3f294e2af932be0e9bcd05bd092ab
kazyhanov_ri <r.kazykhanov@qsoft.ru>
2021-02-14 13:32:47 +0300

    Доработка вывода результатов для более удобного чтения

--- src/ru/heatalways/tasks/SecondTasks.java
+++ src/ru/heatalways/tasks/SecondTasks.java
@@ -10,36 +10,43 @@ public class SecondTasks {
         System.out.println(oppositeHouse(2, 3));
         System.out.println(oppositeHouse(3, 5));
         System.out.println(oppositeHouse(5, 46));
+        System.out.println("-------------------------------------");
 
         // name shuffle
         System.out.println("nameShuffle:");
         System.out.println(nameShuffle("Donald Trump"));
+        System.out.println("-------------------------------------");
 
         // discount
         System.out.println("discount:");
         System.out.println(discount(1500, 50));
+        System.out.println("-------------------------------------");
 
         // difference
         System.out.println("differenceMaxMin:");
         System.out.println(differenceMaxMin(new int[] {10, 4, 1, 4, -10, -50, 32, 21}));
+        System.out.println("-------------------------------------");
 
         // equal
         System.out.println("equal:");
         System.out.println(equal(3, 4, 3));
         System.out.println(equal(1, 1, 1));
         System.out.println(equal(3, 4, 1));
+        System.out.println("-------------------------------------");
 
         // reverse
         System.out.println("reverse:");
         System.out.println(reverse("Hello World!"));
         System.out.println(reverse("The quick brown fox."));
         System.out.println(reverse("Edabit is really helpful!"));
+        System.out.println("-------------------------------------");
 
         // programmers
         System.out.println("programmers:");
         System.out.println(programmers(147, 33, 526));
         System.out.println(programmers(33, 72, 74));
         System.out.println(programmers(1, 5, 9));
+        System.out.println("-------------------------------------");
 
         // getXO
         System.out.println("getXO:");
@@ -47,18 +54,21 @@ public class SecondTasks {
         System.out.println(getXO("xooxx"));
         System.out.println(getXO("ooxXm"));
         System.out.println(getXO("zpzpzpp"));
+        System.out.println("-------------------------------------");
 
         // bomb
         System.out.println("bomb:");
         System.out.println(bomb("There is a bomb."));
         System.out.println(bomb("Hey, did you think there is a BOMB?"));
         System.out.println(bomb("This goes boom!!!"));
+        System.out.println("-------------------------------------");
 
         // sameAscii
         System.out.println("sameAscii:");
         System.out.println(sameAscii("a", "a"));
         System.out.println(sameAscii("AA", "B@"));
         System.out.println(sameAscii("EdAbIt", "EDABIT"));
+        System.out.println("-------------------------------------");
     }
 
     public static int oppositeHouse(int houseNum, int streetLength) {
